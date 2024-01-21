b9824a851896766ca94677eec676ea500018bfa3
PyKnight-vedant <hivedofficial@gmail.com>
2024-01-21 14:12:22 +0530

    changed

--- app.py
+++ app.py
@@ -5,26 +5,28 @@ app = Flask(__name__)
 data = {
     "drinks": [
         {
-            "name": "Grape", 
+            "name": "Grape",
             "description": "Delicious grape fruit drink",
             "date": datetime.now()
-            },
-            {
-            "name": "Lemon", 
+        },
+        {
+            "name": "Lemon",
             "description": "Undiluted lemon fruit drink",
             "date": datetime.now()
-            },
-            {
-            "name": "Mango", 
+        },
+        {
+            "name": "Mango",
             "description": "This is a mango fruit",
             "date": datetime.now()
-            }
+        }
     ]
-} 
+}
+
 
 @app.route("/")
 def index():
-        return "Welcome To My Drinks API"
+    return "Welcome To My Drinks API"
+
 
 @app.route('/drinks')
 def get_drinks():
